666b80728b8df3ec70ef494da4dbd770c337abde
jaanisfehling <jaanisfehling@gmail.com>
2023-07-18 16:59:20 +0200

    add two more datasets

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 - [Heart Disease](https://archive.ics.uci.edu/dataset/45/heart+disease)
 - [Bank Marketing](https://archive.ics.uci.edu/dataset/222/bank+marketing)
 - [Credit Approval](https://archive.ics.uci.edu/dataset/27/credit+approval)
+- [Auction Verification](https://archive.ics.uci.edu/dataset/713/auction+verification)
+- [Abalone Age](https://archive.ics.uci.edu/dataset/1/abalone)
 
 ### Alternative Mixed-Type Clustering Methods
 
@@ -17,3 +19,4 @@
 - DCN
 - DEC
 - IDEC
+- DipDEC
--- load_datasets.py
+++ load_datasets.py
@@ -32,7 +32,7 @@ def load_soybean_disease():
     soybean_df = pd.read_csv("datasets/soybean_disease.csv")
     soybean_df.replace("?", pd.NA, inplace=True)
     soybean_df.dropna(inplace=True)
-    
+
     soybean_y = soybean_df["class"]
     soybean_df.drop(columns=["class"], axis=1, inplace=True)
 
@@ -45,14 +45,15 @@ def load_soybean_disease():
 
 def load_bank_marketing():
     banking_df = pd.read_csv("datasets/bank_marketing.csv", sep=";")
+    banking_df.dropna(inplace=True)
 
     banking_y = banking_df["y"]
     banking_y = LabelEncoder().fit_transform(banking_y)
+    banking_df.drop(columns=["age", "y", "day", "month"], axis=1, inplace=True)
 
     banking_cat_cols = ["job", "marital", "education", "default", "housing", "loan", "contact", "poutcome"]
     banking_cont_cols = ["balance", "duration", "campaign", "pdays", "previous"]
 
-    banking_df.drop(columns=["age", "y", "day", "month"], axis=1, inplace=True)
     banking_df[banking_cat_cols] = banking_df[banking_cat_cols].apply(LabelEncoder().fit_transform)
     banking_df[banking_cont_cols] = StandardScaler().fit_transform(banking_df[banking_cont_cols])
    
@@ -66,11 +67,11 @@ def load_census_income():
     census_df.loc[(census_df["class"] == " <=50K.") | (census_df["class"] == " <=50K"), "class"] = 0
     census_df.loc[(census_df["class"] == " >50K.") | (census_df["class"] == " >50K"), "class"] = 1
     census_y = census_df["class"].to_numpy()
+    census_df.drop(columns=["class"], axis=1, inplace=True)
 
     census_cat_cols = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
     census_cont_cols = ["age", "fnlwgt", "education-num", "capital-gain", "capital-loss", "hours-per-week"]
 
-    census_df.drop(columns="class", inplace=True)
     census_df[census_cat_cols] = census_df[census_cat_cols].apply(LabelEncoder().fit_transform)
     census_df[census_cont_cols] = StandardScaler().fit_transform(census_df[census_cont_cols])
     
@@ -80,14 +81,14 @@ def load_credit_approval():
     credit_df = pd.read_csv("datasets/credit_approval.csv")
     credit_df.replace("?", pd.NA, inplace=True)
     credit_df.dropna(inplace=True)
-    
+
     credit_y = credit_df["A16"]
     credit_y = LabelEncoder().fit_transform(credit_y)
+    credit_df.drop(columns=["A16"], axis=1, inplace=True)
 
     credit_cat_cols = ["A1", "A4", "A5", "A6", "A7", "A9", "A10", "A12", "A13"]
     credit_cont_cols = ["A2", "A3", "A8", "A11", "A14", "A15"]
 
-    credit_df.drop(columns="A16", inplace=True)
     credit_df[credit_cat_cols] = credit_df[credit_cat_cols].apply(LabelEncoder().fit_transform)
     credit_df[credit_cont_cols] = StandardScaler().fit_transform(credit_df[credit_cont_cols])
         
@@ -98,14 +99,42 @@ def load_heart_disease():
     heart_df.dropna(inplace=True)
 
     heart_y = heart_df["num"].to_numpy()
+    heart_df.drop(columns=["id", "dataset", "num"], axis=1, inplace=True)
 
     heart_cat_cols = ["sex", "cp", "fbs", "restecg", "exang", "slope", "thal"]
     heart_cont_cols = ["age", "trestbps", "chol", "thalch", "oldpeak", "ca"]
-
-    heart_df.drop(columns=["id", "dataset", "num"], inplace=True)
-    heart_df.dropna(inplace=True)
 
     heart_df[heart_cat_cols] = heart_df[heart_cat_cols].apply(LabelEncoder().fit_transform)
     heart_df[heart_cont_cols] = StandardScaler().fit_transform(heart_df[heart_cont_cols])
     
     return TabularDataSet("Heart Disease", heart_df, heart_y, heart_cat_cols, heart_cont_cols)
+
+def load_auction_verification():
+    auction_df = pd.read_csv("datasets/auction_verification.csv")
+    auction_df.dropna(inplace=True)
+
+    auction_y = auction_df["verification.result"].to_numpy()
+    auction_df.drop(columns=["verification.result", "verification.time"], axis=1, inplace=True)
+
+    auction_cat_cols = ["process.b1.capacity", "process.b2.capacity", "process.b3.capacity", "process.b4.capacity", "property.product", "property.winner"]
+    auction_cont_cols = ["property.price"]
+
+    auction_df[auction_cat_cols] = auction_df[auction_cat_cols].apply(LabelEncoder().fit_transform)
+    auction_df[auction_cont_cols] = StandardScaler().fit_transform(auction_df[auction_cont_cols])
+
+    return TabularDataSet("Auction Verification", auction_df, auction_y, auction_cat_cols, auction_cont_cols)
+
+def load_abalone_age():
+    abalone_df = pd.read_csv("datasets/abalone_age.csv")
+    abalone_df.dropna(inplace=True)
+
+    abalone_y = abalone_df["Rings"].to_numpy()
+    abalone_df.drop(columns=["Rings"], axis=1, inplace=True)
+
+    abalone_cat_cols = ["Sex"]
+    abalone_cont_cols = ["Length", "Diameter", "Height", "Whole_weight", "Shucked_weight", "Viscera_weight", "Shell_weight"]
+
+    abalone_df[abalone_cat_cols] = abalone_df[abalone_cat_cols].apply(LabelEncoder().fit_transform)
+    abalone_df[abalone_cont_cols] = StandardScaler().fit_transform(abalone_df[abalone_cont_cols])
+
+    return TabularDataSet("Abalone Age", abalone_df, abalone_y, abalone_cat_cols, abalone_cont_cols)
